502d81d94ada1248c7a03601e0d98215faa3fd69
laushinka <laurie.malau@gmail.com>
2016-09-07 17:33:13 -0400

    Add #initialize, #add_review, .find_by_name

--- object_relations/app/models/customer.rb
+++ object_relations/app/models/customer.rb
@@ -1,8 +1,33 @@
 class Customer
-  
+  attr_accessor :name, :reviews
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @reviews = []
+    @@all << self
+  end
+
+  def add_review(review)
+    @reviews << review
+    review.customer = self
+  end
+
+  def self.all=(all)
+    @@all = all
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |customer|
+      customer.name == name
+    end
+  end
 end
 
 # customer = customer.add_review('it was good', Restaurant.new)
 # a new review tied ot the restaurant
 # and tied to the customer
-
