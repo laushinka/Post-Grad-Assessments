8094fc4a16b4846e1a7ef456902612f0451af4bc
laushinka <laurie.malau@gmail.com>
2016-09-07 19:24:51 -0400

    Fill in #owner

--- sql-relations/app/models/restaurant.rb
+++ sql-relations/app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant
   include Databaseable::InstanceMethods
   extend Databaseable::ClassMethods
-  
+
   ATTRIBUTES = {
     id: "INTEGER PRIMARY KEY",
     name: "TEXT",
@@ -9,9 +9,15 @@ class Restaurant
     owner_id: "INTEGER"
   }
 
-  attr_accessor(*self.public_attributes)  
+  attr_accessor(*self.public_attributes)
   attr_reader :id
 
   def owner
+    sql = <<-SQL
+      SELECT owners.* FROM restaurants
+      JOIN owners ON restaurants.owner_id = owners.id
+      WHERE restaurants.id = ?
+    SQL
+    self.class.db.execute(sql, self.id)
   end
 end
