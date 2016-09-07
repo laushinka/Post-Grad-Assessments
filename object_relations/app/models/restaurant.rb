81bedbfe22441d6d2d2a3fba07dff8fe360e7b93
laushinka <laurie.malau@gmail.com>
2016-09-07 17:33:34 -0400

    Add #add_review, #customers

--- object_relations/app/models/restaurant.rb
+++ object_relations/app/models/restaurant.rb
@@ -1,4 +1,24 @@
-class Restaurant
+# Restaurant has many customers
+# Restaurant has many reviews through customers
 
-  
+class Restaurant
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @reviews = []
+    @@all << self
+  end
+
+  def add_review(review)
+    @reviews << review
+  end
+
+  def customers
+    @reviews.collect do |review|
+      review.customer
+    end
+  end
+
 end
