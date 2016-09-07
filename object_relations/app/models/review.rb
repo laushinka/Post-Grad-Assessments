b53cebc86d6da2f4e6dd7a0a44d087877bf13c6d
laushinka <laurie.malau@gmail.com>
2016-09-07 17:34:00 -0400

    Add #initialize

--- object_relations/app/models/review.rb
+++ object_relations/app/models/review.rb
@@ -1,4 +1,13 @@
-class Review
-  
-end
+# Review belongs to a customer
+# Review belongs to a restaurant
+# Review knows about the restaurant that it belongs to
 
+class Review
+  attr_accessor :title, :customer, :restaurant
+
+  def initialize(title, restaurant)
+    @title = title
+    @restaurant = restaurant
+    restaurant.add_review(self) # From Restaurant.add_review
+  end
+end
