7670ec69364fc50ed56fe0bfb766c355aad84329
laushinka <laurie.malau@gmail.com>
2016-09-08 00:06:27 -0400

    Add seeds

---
+++ javascript/seed.js
@@ -0,0 +1,11 @@
+var lala = new Customer("Lala");
+var tommy = new Customer("Tommy");
+var luna = new Customer("Luna");
+
+var bk = new Restaurant("Burger King");
+var wb = new Restaurant("White Bear");
+var mcd = new Restaurant("McDonalds");
+
+var rev1 = new Review("Lame", lala, bk);
+var rev2 = new Review("Kewl", tommy, wb);
+var rev3 = new Review("Woh", luna, mcd);
